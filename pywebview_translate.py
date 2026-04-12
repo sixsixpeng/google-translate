0bf1e20497fcc59bdaf98a62215a1896bd44cf09
我胖虎不打马云 <sixsixsixpeng@qq.com>
2026-04-12 23:23:26 +0800

    optimization and update

--- pywebview_translate.py
+++ pywebview_translate.py
@@ -10,7 +10,13 @@ window = webview.create_window(
     url="https://google-translate-proxy.tantu.com/",
     width=800,
     height=500,
-    on_top=True
+    on_top=True,
+    text_select=True,
+    zoomable=True,
+    min_size=(600, 500),
 )
 
-webview.start(debug=False)
+webview.start(debug=False,
+              private_mode=False,
+              storage_path=f'./cache/'
+              )
