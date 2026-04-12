b1c6bc160e12ac1bd54d8e0d8fea7caa3ffdec4e
我胖虎不打马云 <sixsixsixpeng@qq.com>
2026-04-12 15:48:04 +0800

    pywebview_translate

---
+++ pywebview_translate.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+# 2026/4/12 15:33
+
+# pyinstaller -D -w -i icon.ico m3.py
+import webview
+
+# 创建窗口
+window = webview.create_window(
+    title="事件演示窗口",
+    url="https://google-translate-proxy.tantu.com/",
+    width=800,
+    height=500,
+    on_top=True
+)
+
+webview.start(debug=False)
